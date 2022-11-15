7acb4dc4c7f6a7a11c4710d3a47d02f74b5c2ab8
shoxa <antihero752@gmail.com>
2022-11-15 20:50:31 +0500

    first

---
+++ app.js
@@ -0,0 +1,34 @@
+// let incr = 10,
+//     decr = 10;
+//
+// // ++incr
+// // --decr
+//
+// console.log(incr)
+// console.log (decr)
+
+
+
+
+
+
+
+
+
+
+
+const numberOfFilms = +prompt('сколько фильмов вы просмотрели?', '')
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movie: {},
+        actors: {},
+        genres: [],
+        privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '')
+z
