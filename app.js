774644e2559b0c15bb9b70f7ec46135806b867d6
shoxa <antihero752@gmail.com>
2022-11-16 20:41:28 +0500

    all commits

--- app.js
+++ app.js
@@ -12,11 +12,6 @@
 
 
 
-
-
-
-
-
 const numberOfFilms = +prompt('сколько фильмов вы просмотрели?', '')
 
 const personalMovieDB = {
@@ -26,15 +21,45 @@ const personalMovieDB = {
         genres: [],
         privat: false
 }
+//
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '')
+//
+//
+// personalMovieDB.movie[a] = b
+// personalMovieDB.movie[c] = d
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '')
+
+for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '')
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movie[a] = b
+                console.log('done')
+        } else  {
+                console.log('error')
+                i--
+        }
+
+}
 
 
-personalMovieDB.movie[a] = b
-personalMovieDB.movie[c] = d
+if (personalMovieDB.count < 10) {
+        console.log('мало')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('киноман')
+} else {
+        console.log('ошибка')
+}
+
+
+
+
+
+
+
 
 
 console.log(personalMovieDB)
